4f51fd52909699b5b237e9d6075e1275f25db119
Nazar Dzhuruk <nazar.dzhuruk@gmail.com>
2021-12-17 03:59:38 +0100

    Hide private data

--- src/main/java/com/foxminded/telebot/source/BotConfig.java
+++ src/main/java/com/foxminded/telebot/source/BotConfig.java
@@ -1,6 +1,6 @@
 package com.foxminded.telebot.source;
 
 public class BotConfig {
-    public static final String BOT_TOKEN = "5026460657:AAFQbKA9MPt7fA4F8z31N9-CLycPbhq2P2Q";
-    public static final String BOT_ID = "@nzik_bot";
+    public static final String BOT_TOKEN = "YOUR BOT TOKEN";
+    public static final String BOT_ID = "YOUR BOT NAME";
 }
